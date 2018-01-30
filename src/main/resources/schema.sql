762a0910638ca32361565695f7d65dda6cf2ab21
libing <lb_special@163.com>
2018-01-30 22:44:38 +0800

    更新pom文件，schema文件。

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -23,3 +23,13 @@ CREATE TABLE IF NOT EXISTS company (
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COMMENT '消费公司';
 
 INSERT INTO company(id, name, tax_no, address, business, is_country, contact_person, office, mobile_phone, telephone, sex, create_time, need, enable) VALUES ();
+
+-- 创建客户表
+CREATE TABLE IF NOT EXISTS customer (
+  id BIGINT NOT NULL AUTO_INCREMENT COMMENT '客户ID',
+  name VARCHAR(60) NOT NULL COMMENT '客户名称',
+  create_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  enable BOOLEAN NOT NULL DEFAULT TRUE COMMENT '是否有效'
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COMMENT '客户表';
+
+-- 创界客户联系人表
