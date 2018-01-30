a9bdbc2032110adc8b158e8d53d1418ecb7a1205
libing <lb_special@163.com>
2018-01-31 00:29:25 +0800

    表结构修改。

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -22,14 +22,27 @@ CREATE TABLE IF NOT EXISTS company (
   PRIMARY KEY (id)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COMMENT '消费公司';
 
-INSERT INTO company(id, name, tax_no, address, business, is_country, contact_person, office, mobile_phone, telephone, sex, create_time, need, enable) VALUES ();
+# INSERT INTO company(id, name, tax_no, address, business, is_country, contact_person, office, mobile_phone, telephone, sex, create_time, need, enable) VALUES ();
 
 -- 创建客户表
 CREATE TABLE IF NOT EXISTS customer (
   id BIGINT NOT NULL AUTO_INCREMENT COMMENT '客户ID',
   name VARCHAR(60) NOT NULL COMMENT '客户名称',
   create_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
-  enable BOOLEAN NOT NULL DEFAULT TRUE COMMENT '是否有效'
+  enable BOOLEAN NOT NULL DEFAULT TRUE COMMENT '是否有效',
+  PRIMARY KEY (id)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COMMENT '客户表';
+
+-- 创建开票信息表
+CREATE TABLE IF NOT EXISTS invoice_information (
+  customer_id BIGINT NOT NULL COMMENT '客户ID',
+  name VARCHAR(60) NOT NULL COMMENT '抬头',
+  unique_id VARCHAR(18) COMMENT '唯一ID',
+  address VARCHAR(300) COMMENT '地址',
+  telephone VARCHAR(15) COMMENT '电话',
+  opening_bank VARCHAR(60) COMMENT '开户银行',
+  bank_no VARCHAR(20) COMMENT '开户银行卡号',
+  KEY idx_customer_id (customer_id)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COMMENT '开票信息表';
 
 -- 创界客户联系人表
